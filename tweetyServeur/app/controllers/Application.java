bb4acd88dbdb8ae606b2432d4c1df3b118e5bc4a
Arihy <ynallem@gmail.com>
2013-09-22 23:02:53 +0200

    wall controller

--- tweetyServeur/app/controllers/Application.java
+++ tweetyServeur/app/controllers/Application.java
@@ -1,14 +1,19 @@
 package controllers;
 
+import models.Tweet;
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+    
+    public static Result allTweet()
+    {
+    	return ok(views.html.wall.render(Tweet.findAll()));
     }
 
 }
