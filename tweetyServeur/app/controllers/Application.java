614290b7d66e3a0ac69bba940234960056c8f751
Arihy <ynallem@gmail.com>
2013-09-22 23:58:19 +0200

    form add tweet

--- tweetyServeur/app/controllers/Application.java
+++ tweetyServeur/app/controllers/Application.java
@@ -1,11 +1,15 @@
 package controllers;
 
+import play.data.Form;
+
 import models.Tweet;
 import play.*;
 import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
+	
+	final static Form<Tweet> tweetForm = Form.form(Tweet.class);
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
@@ -13,7 +17,7 @@ public class Application extends Controller {
     
     public static Result allTweet()
     {
-    	return ok(views.html.wall.render(Tweet.findAll()));
+    	return ok(views.html.wall.render(Tweet.findAll(), tweetForm));
     }
 
 }
