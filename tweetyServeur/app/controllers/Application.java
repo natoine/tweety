4ff158e498d87473e4947fd46188f4c14f9a85c5
natoine <natoine@natoine-laptop.(none)>
2013-10-06 23:17:27 +0200

    modified routes and views

--- tweetyServeur/app/controllers/Application.java
+++ tweetyServeur/app/controllers/Application.java
@@ -1,39 +1,47 @@
 package controllers;
 
-
 import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
-import play.data.Form;
 import models.Tweet;
+
 import play.*;
 import play.libs.Json;
 import play.mvc.*;
+
 import views.html.*;
-import play.data.Form;
 
 public class Application extends Controller {
-	
-	final static Form<Tweet> tweetForm = Form.form(Tweet.class);
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
-    
-    public static Result allTweet()
-    {
-    	if (request().accepts("text/html")) return ok(views.html.wall.render(Tweet.findAll(), tweetForm));
-    	else if(request().accepts("application/json")) return ok(Json.toJson(Tweet.findAll()));
-    	return badRequest();
+        return ok(index.render());
     }
 
-    public static Result createTweet()
+    public static Result listTweets(String username)
     {
-    	Form<Tweet> form = Form.form(Tweet.class).bindFromRequest();
-    	Tweet tweet = new Tweet();
-    	tweet.setComment(form.field("comment").value());
-    	tweet.setUsername(form.field("username").value());
-    	tweet.setCreationDate(new Date());
-    	Tweet.create(tweet);
-    	return redirect(routes.Application.allTweet());
+    	//TODO remove test and link to model
+    	//Just for the test
+    	List<Tweet> tweets = new ArrayList<Tweet>();
+    	Tweet tweet1 = new Tweet();
+    	tweet1.commentaire = "bla bla bla" ;
+    	tweet1.username = "toto" ;
+    	tweet1.creationDate = new Date();
+    	tweets.add(tweet1);
+    	
+    	Tweet tweet2 = new Tweet();
+    	tweet2.commentaire = "again bla bla bla" ;
+    	tweet2.username = "lulu" ;
+    	tweet2.creationDate = new Date();
+    	tweets.add(tweet2);
+    	
+    	if(request().accepts("text/html"))
+    		return ok(views.html.wall.render(tweets, "bonjour " + username));
+    	else if(request().accepts("application/json"))
+    		return ok(Json.toJson(tweets));
+    	else if (request().accepts("application/rdf+xml"))
+    		return ok("this will be RDF XML");
+    	return badRequest();
     }
+    
 }
--- tweetyServeur/app/models/Tweet.java
+++ tweetyServeur/app/models/Tweet.java
@@ -1,12 +1,9 @@
 package models;
 
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
-import com.avaje.ebean.Ebean;
 
 import play.db.ebean.Model;
 
@@ -14,43 +11,9 @@ import play.db.ebean.Model;
 public class Tweet extends Model
 {
 	@Id
-	public long id;
+	public long id ;
+	public String commentaire ;
+	public String username ;
+	public Date creationDate ;
 	
-	public String comment;
-	public String username;
-	public Date creationDate;
-	
-	public static Finder<Long, Tweet> find = new Finder<Long, Tweet>(Long.class, Tweet.class);
-	
-	public static List<Tweet> findAll()
-	{
-		return find.all();
-	}
-	
-	public static void create(Tweet tweet)
-	{
-		Ebean.save(tweet);
-	}
-	
-	public String getComment() {
-		return comment;
-	}
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public Date getCreationDate() {
-		return creationDate;
-	}
-	public void setCreationDate(Date creationDate) {
-		this.creationDate = creationDate;
-	}
-	public long getId() {
-		return id;
-	}
 }
